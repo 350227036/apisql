e964041f6321a822dba538522cee7aa9cc2cd0d1
kerin <350227036@qq.com>
2020-06-07 14:18:13 +0800

    mybatis-generaoter

---
+++ src/main/java/com/kerin/apisql/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.kerin.apisql.dao;
+
+import com.kerin.apisql.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
